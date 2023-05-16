5ea96c067f48f3d417b1288c6e52bdbe05c10344
AddictivelyRecursive <133158981+AddictivelyRecursive@users.noreply.github.com>
2023-05-17 02:30:04 +0530

    Add files via upload

---
+++ interesting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class interesting {
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=in.nextInt();
+            System.out.println((n+1)/10);
+        }
+    }   
+}
